cea0d17511a520051dc120fb2a9ec84c9bcacef3
mikolaj <mikolaj@rccloud.pl>
2020-03-26 22:53:56 +0100

    Refactoring #1

--- wav-cutter.py
+++ wav-cutter.py
@@ -1,31 +1,63 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
-import sys
-import math
 import wave
-import argparse
-import contextlib
 
-parser = argparse.ArgumentParser(description="Simple wav file cutter")
-parser.add_argument('footage_path', type=str)
-parser.add_argument('max_duration', type=int)
-args = parser.parse_args()
+def cut_footage(footage_path, max_duration, footage_dest=None):
+	'''
+	Cut footage file to max duration in seconds
 
-with contextlib.closing(wave.open(args.footage_path, 'rb')) as cf:
-	frames = cf.getnframes()
-	rate = float(cf.getframerate())
-
-	max_frames = int(math.ceil(args.max_duration * rate))
-
-	if max_frames > frames:
-		sys.exit("Footage is to short")
-
-	with contextlib.closing(wave.open(args.footage_path + '.new', 'wb')) as nf:
-		nf.setnchannels(cf.getnchannels())
-		nf.setsampwidth(cf.getsampwidth())
-		nf.setframerate(cf.getframerate())
-
-		nf.writeframes(cf.readframes(max_frames))
-
-print "\n[!] Created new file=%s\n" % args.footage_path + ".new"
+	Args:
+		footage_path (str): Path to footage file
+		max_duration (int): Max duration of footage (in seconds)
+		footage_dest (str): Destination path to new footage (default: None)
 	
+	Raises:
+		IOError: Files I/O problem
+		wave.Error: Wave library errors
+
+	'''
+	import math
+	from contextlib import closing
+
+	destination_footage_path = footage_dest or footage_path + '.new'
+
+	with closing(wave.open(footage_path, 'rb')) as cf:
+		frames = cf.getnframes()
+		rate = float(cf.getframerate())
+		max_frames = int(math.ceil(max_duration * rate))
+
+		if max_frames > frames:
+			max_frames = frames
+
+		with closing(wave.open(destination_footage_path, 'wb')) as nf:
+			nf.setnchannels(cf.getnchannels())
+			nf.setsampwidth(cf.getsampwidth())
+			nf.setframerate(cf.getframerate())
+			nf.writeframes(cf.readframes(max_frames))
+
+def main():
+	import os
+	import sys
+	import argparse
+
+	parser = argparse.ArgumentParser(description="Simple wav file cutter")
+	parser.add_argument('footage_path', type=str)
+	parser.add_argument('max_duration', type=int)
+	parser.add_argument('--footage_dest', type=str, default=None)
+	args = parser.parse_args()
+
+	try:
+		if not os.path.exists(args.footage_path):
+			sys.exit("File %s not exists" % args.footage_path)
+
+		if not os.access('.', os.W_OK):
+			sys.exit("This directory is not writable")
+
+		cut_footage(args.footage_path, args.max_duration, args.footage_dest)
+	except IOError as x:
+		sys.exit(x)
+	except wave.Error as x:
+		sys.exit(x)
+
+if __name__ == "__main__":
+	main()
